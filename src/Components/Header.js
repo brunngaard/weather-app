afb1314d3a1591f9de65cf1f63ff16f675bb99cb
e-brunngard <brunngard.emil@gmail.com>
2021-03-28 05:00:02 +0200

    weather app start

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1>Wheater App</h1>
+    </div>
+  );
+};
+
+export default Header;
